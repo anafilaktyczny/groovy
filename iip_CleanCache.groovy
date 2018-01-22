91d489ad9bfad072bd7e4b6038741fb5d54ef856
Grzegorz Hetman <grzegorz.hetman@tieto.com>
2018-01-22 09:01:41 +0100

    Fix vars order

--- iip_CleanCache.groovy
+++ iip_CleanCache.groovy
@@ -2,9 +2,9 @@ node {
   // Remove obsolete files based on atime timestamp.
   if (params.REMOVE_OBSOLETES){
     stage('Remove obsolete files.') {
-      println("Find all files older then ${days} and delete them")
       def cache_dir="/tmp/"
       def days=19
+      println("Find all files older then ${days} and delete them")
       dir("${env.WORKSPACE}/../${env.JOB_NAME}@script/scripts/"){
         if (params.DEBUG){
           sh (returnStdout: true, script: "./iip_CleanCache.sh -a delete_it -d ${cache_dir} -n ${days} -v")
