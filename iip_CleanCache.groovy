448b2f2ddeea9ed67e2c38cae95673d1f1b4f694
Grzegorz Hetman <grzegorz.hetman@tieto.com>
2018-01-22 13:43:20 +0100

    TesT

--- iip_CleanCache.groovy
+++ iip_CleanCache.groovy
@@ -1,5 +1,55 @@
 node('slave-agent_ea62956482df') {
+
+  state('Bootstrap'){
+    // Access to the Hudson Singleton
+    hudsonInstance = hudson.model.Hudson.instance
+
+    // Retrieve matching jobs
+    allItems = hudsonInstance.items
+    chosenJobs = allItems.findAll{job -> job.name =~ /$jobFilter/}
+
+// Do work and create the result table
+chosenJobs.each { job ->
+  if(!(job instanceof hudson.model.ExternalJob)) {
+
+    // No SCM-Configuration possible for External Jobs!
+    //if (job.scm instanceof SubversionSCM) { 
+      println ""
+      println job.name
+      
+      // Job has a SubversionSCM-Configuration
+      def newSvnPath = [][]
+
+      job.scm.locations.each{
+        //For every Subversion-Location
+        println "-   $it.remote"
+        println "-   $it"
+        /*
+        if (it.remote =~ /$urlFilter/) {
+          //SVN-Path contains the given Path
+          newRemote = it
+          match =  it.remote =~ /$oldBranch/
+          
+          if(match) {
+            newRemote = match.replaceFirst(newBranch)
+            println " -> $newRemote"
+            newSvnPath.add(it.withRemote(newRemote))
+          } else {
+            println "    Doesn't match oldBranch"
+            newSvnPath.add(it)
+          }
+
+        } else {
+          println "    Doesn't match urlFilter"
+        }
+        */
+      }
+    }  
+  //}
+  
   // Remove obsolete files based on atime timestamp.
+
+  
   if (params.REMOVE_OBSOLETES){
     stage('Remove obsolete files.') {
       def cache_dir="/tmp/"
