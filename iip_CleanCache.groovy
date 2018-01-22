0e85202a193062423e5df2ac4f9692478fa3c60c
Grzegorz Hetman <grzegorz.hetman@tieto.com>
2018-01-22 13:50:10 +0100

    TesT

--- iip_CleanCache.groovy
+++ iip_CleanCache.groovy
@@ -44,22 +44,19 @@ node('slave-agent_ea62956482df') {
         }
         */
       }
-    }  
-  //}
+    }
   
-  // Remove obsolete files based on atime timestamp.
-
-  
-  if (params.REMOVE_OBSOLETES){
-    stage('Remove obsolete files.') {
-      def cache_dir="/tmp/"
-      def days=19
-      println("Find all files older then ${days} and delete them")
-      dir("${env.WORKSPACE}/../${env.JOB_NAME}@script/scripts/"){
-        if (params.DEBUG){
-          sh (returnStdout: true, script: "./iip_CleanCache.sh -a delete_it -d ${cache_dir} -n ${days} -v")
-        } else {
-          sh (returnStdout: false, script: "./iip_CleanCache.sh -a delete_it -d ${cache_dir} -n ${days}")
+    if (params.REMOVE_OBSOLETES){
+      stage('Remove obsolete files.') {
+        def cache_dir="/tmp/"
+        def days=19
+        println("Find all files older then ${days} and delete them")
+        dir("${env.WORKSPACE}/../${env.JOB_NAME}@script/scripts/"){
+          if (params.DEBUG){
+            sh (returnStdout: true, script: "./iip_CleanCache.sh -a delete_it -d ${cache_dir} -n ${days} -v")
+          } else {
+            sh (returnStdout: false, script: "./iip_CleanCache.sh -a delete_it -d ${cache_dir} -n ${days}")
+          }
         }
       }
     }
