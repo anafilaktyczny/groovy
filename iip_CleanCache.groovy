abe7914a0df4c20f9af5c7d9211f3124010709a1
anafilaktyczny <grzegorz.hetman@tieto.com>
2018-01-23 08:48:36 +0100

    TEST1

--- iip_CleanCache.groovy
+++ iip_CleanCache.groovy
@@ -3,17 +3,6 @@
 import hudson.scm.*
 node {
   // Access to the Hudson Singleton
-  hudsonInstance = hudson.model.Hudson.instance
-  // Retrieve matching jobs
-  allItems = hudsonInstance.items
-  //jobFilter = env.JOB_NAME
-  //chosenJobs = allItems.findAll{job -> job.name =~ /$jobFilter/}
-  // Do work and create the result table
-  allItems.each { job ->
-    //if(!(job instanceof hudson.model.ExternalJob)) {
-      println "HI:-> "
-      println job.name
-  //  }
-  }
+  echo getUserRemoteConfigs();
 
 }
