4f96a7f16dd546a98c03a0e3366e5438a5f65f7e
anafilaktyczny <grzegorz.hetman@tieto.com>
2018-01-23 08:49:19 +0100

    TEST1

--- iip_CleanCache.groovy
+++ iip_CleanCache.groovy
@@ -1,8 +1,8 @@
 //node('slave-agent_ea62956482df') {
 
-import hudson.scm.*
 node {
   // Access to the Hudson Singleton
-  echo getUserRemoteConfigs();
+  import hudson.*
+  echo scm.getUserRemoteConfigs()
 
 }
